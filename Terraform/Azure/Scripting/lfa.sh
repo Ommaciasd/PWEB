6fb27f7a8fcef7d058f872a5037bfe3c8d30e584
Oscar Macias <79873527+OMaciasd@users.noreply.github.com>
2023-08-25 11:03:38 -0500

    :ambulance: cd(main) Change lfa.sh - $ENV-functions
    :ambulance: cd(main) Change lfa.sh - $ENV-functions

--- Terraform/Azure/Scripting/lfa.sh
+++ Terraform/Azure/Scripting/lfa.sh
@@ -4,10 +4,15 @@
 funtionappName=
 RESOURCEGROUP=rginfradevops
 
-# ENV - SERVICE PLAN.
+# SERVICE PLAN.
 SERVICEPLAN_DEV=app-dev
 SERVICEPLAN_STA=app-sta
 SERVICEPLAN_PRD=app-prd
+
+# STORAGE ACCOUNT.
+STORAGE_DEV=dev${funtionappName}functions
+STORAGE_STA=sta${funtionappName}functions
+STORAGE_PROD=prd${funtionappName}functions
 
 # AZURE RESOURCES.
 az account set --subscription "$AZURE_SUSCRIPTION"
@@ -19,21 +24,21 @@ az appservice plan create --name "$SERVICEPLAN_STA" --resource-group "$RESOURCEG
 az appservice plan create --name "$SERVICEPLAN_PRD" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location 'eastus'
 
 # AZURE RESOURCES: STORAGE ACCOUNT.
-az storage account create --name "dev$funtionappName" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=dev
-az storage account create --name "sta$funtionappName" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=sta
-az storage account create --name "prd$funtionappName" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=prod
+az storage account create --name "$STORAGE_DEV" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=dev
+az storage account create --name "$STORAGE_STA" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=sta
+az storage account create --name "$STORAGE_PROD" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=prod
 
 # DEV ENVIRONMENT.
 az functionapp create --resource-group "$RESOURCEGROUP" \
---runtime dotnet-isolated --functions-version 4 --name "dev-$funtionappName" --storage-account "dev$funtionappName" --os-type Linux --plan "$SERVICEPLAN_DEV" --tags created_by=azure environment=dev
-az functionapp config appsettings set --name "dev-$funtionappName" --resource-group "$RESOURCEGROUP" --settings TZ="America/Lima"
+--runtime dotnet-isolated --functions-version 4 --name "dev-nanaykuna-$funtionappName-functions" --storage-account "$STORAGE_DEV" --os-type Linux --plan "$SERVICEPLAN_DEV" --tags created_by=azure environment=dev
+az functionapp config appsettings set --name "dev-nanaykuna-$funtionappName-functions" --resource-group "$RESOURCEGROUP" --settings TZ="America/Lima"
 
 # STA ENVIRONMENT.
 az functionapp create --resource-group "$RESOURCEGROUP" \
---runtime dotnet-isolated --functions-version 4 --name "sta-$funtionappName" --storage-account "sta$funtionappName" --os-type Linux --plan "$SERVICEPLAN_STA" --tags created_by=azure environment=sta
-az functionapp config appsettings set --name "dev-$funtionappName" --resource-group "$RESOURCEGROUP" --settings TZ="America/Lima"
+--runtime dotnet-isolated --functions-version 4 --name "sta-nanaykuna-$funtionappName-functions" --storage-account "$STORAGE_STA" --os-type Linux --plan "$SERVICEPLAN_STA" --tags created_by=azure environment=sta
+az functionapp config appsettings set --name "dev-nanaykuna-$funtionappName-functions" --resource-group "$RESOURCEGROUP" --settings TZ="America/Lima"
 
 # PROD ENVIRONMENT.
 az functionapp create --resource-group "$RESOURCEGROUP" \
---runtime dotnet-isolated --functions-version 4 --name "prd-$funtionappName" --storage-account "prd$funtionappName" --os-type Linux --plan "$SERVICEPLAN_PRD" --tags created_by=azure environment=prod
-az functionapp config appsettings set --name "dev-$funtionappName" --resource-group "$RESOURCEGROUP" --settings TZ="America/Lima"
+--runtime dotnet-isolated --functions-version 4 --name "prd-nanaykuna-$funtionappName-functions" --storage-account "$STORAGE_PROD" --os-type Linux --plan "$SERVICEPLAN_PRD" --tags created_by=azure environment=prod
+az functionapp config appsettings set --name "dev-nanaykuna-$funtionappName-functions" --resource-group "$RESOURCEGROUP" --settings TZ="America/Lima"
