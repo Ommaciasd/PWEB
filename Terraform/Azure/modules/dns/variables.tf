6b7071e7dfdf1207b87e417c31356e12c94f8c15
Oscar Macias <79873527+OMaciasd@users.noreply.github.com>
2023-11-17 00:59:26 -0500

    ✨ feat(tf): Change dns mod 4 az prod env

--- Terraform/Azure/modules/dns/variables.tf
+++ Terraform/Azure/modules/dns/variables.tf
@@ -9,6 +9,24 @@ variable "group"       { type = string }
 variable "private"     { type = string }
 variable "environment" { type = string }
 
+variable "dns_zone_name" {
+  type        = string
+  default     = null
+  description = "Name of the DNS zone."
+}
+
+variable "dns_ttl" {
+  type        = number
+  default     = 3600
+  description = "Time To Live (TTL) of the DNS record (in seconds)."
+}
+
+variable "create_dns_resource" {
+  description = "Flag to determine if DNS resource should be created."
+  type        = bool
+  default     = false
+}
+
 locals {
   ttl         = "300"
   test        = "test"
@@ -22,39 +40,3 @@ locals {
   email       = "azuredns-hostmaster.microsoft.com"
   value       = "BE59ABCD5CA8C90D1A3AA0D6B37FCA7854E9A5AC000451FD8B8221B6B4C69B5D"
 }
-
-variable "dns_ttl" {
-  type        = number
-  default     = 3600
-  description = "Time To Live (TTL) of the DNS record (in seconds)."
-}
-
-variable "dns_records" {
-  type        = list(string)
-  default     = ["1.2.3.4", "1.2.3.5"]
-  description = "List of IPv4 addresses."
-}
-
-variable "dns_zone_name" {
-  type        = string
-  default     = null
-  description = "Name of the DNS zone."
-}
-
-#-------------------------------------------------------------
-
-locals {
-  # refix for naming convention across all resources
-  resource_prefix = "az104-04-"
-
-  # ip addresses for x2 deployed subnets, iterated over with for_each block in network.tf
-  subnet_ips = ["10.40.0.0/24", "10.40.1.0/24"]
-
-  common_tags = {
-    env   = "training"
-    lab   = "az-104-lab-4"
-    owner = "zimcanit"
-  }
-}
-
-#-------------------------------------------------------------
