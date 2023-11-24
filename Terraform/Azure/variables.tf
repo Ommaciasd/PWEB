fb783a9685f45c34591e4c4b45fdae0fc8a00bae
Oscar Macias <79873527+OMaciasd@users.noreply.github.com>
2023-11-24 12:50:10 -0500

    Update variables.tf

--- Terraform/Azure/variables.tf
+++ Terraform/Azure/variables.tf
@@ -1,28 +1,11 @@
-variable "plan" {
+variable "$VARIABLE_NAME" {}
+
+variable "$VARIABLE_NAME_LIST" {
   type    = list(string)
-  default = ["webs", "functions"]
+  default = ["$ITEM"]
 }
 
-locals {
-  db          = var.db
-  vm          = var.vm
-  lvmss       = var.lvmss
-  dns         = var.dns
-  key         = var.key
-  sql         = var.sql
-  apps        = var.apps
-  pdns        = var.pdns
-  tenant       = var.tenant
-  destroy      = var.destroy
-  security     = var.security
-  network      = var.network
-  resources    = var.resources
-  aspnetcore   = var.aspnetcore
-  environment  = var.environment
-  subscription = var.subscription
-}
-
-variable "gateway" {
+variable "VARIABLE_NAME_OBJECT" {
   type = list(object({
     name           = string
     address_prefix = string
@@ -30,105 +13,8 @@ variable "gateway" {
   default = [
 
     {
-      name           = "gw-01"
-      address_prefix = "10.10.0.0/24"
-    },
-
-    {
-      name           = "functions"
-      address_prefix = "10.10.2.0/24"
+      name           = "$OBJECT_LIST_NAME"
+      address_prefix = "$NETWORK/$CIDR"
     }
   ]
-}
-
-variable "dns" {
-  type = list(object({
-    name      = string
-    host_name = string
-  }))
-  default = [
-
-    {
-      name      = "$DOMAIN_WEB.$ROOT"
-      host_name = "ns1-35.azure-dns.com"
-    },
-
-    {
-      name      = "$DOMAIN_WEB_2.$ROOT"
-      host_name = "ns1-34.azure-dns.com"
-    },
-
-    {
-      name      = "$DOMAIN_WEB_2.com"
-      host_name = "ns1-33.azure-dns.com"
-    },
-
-    {
-      name      = "$DOMAIN_WEB_3.com"
-      host_name = "ns1-37.azure-dns.com"
-    }
-  ]
-}
-
-variable "subnet" {
-  type = list(object({
-    name           = string
-    address_prefix = string
-  }))
-  default = [
-
-    {
-      name           = "snet-01"
-      address_prefix = "10.10.1.0/24"
-    },
-
-    {
-      name           = "vms"
-      address_prefix = "10.10.3.0/24"
-    }
-  ]
-}
-
-variable "destroy" {}
-variable "resources" {}
-variable "db" { type = string }
-variable "vm" { type = string }
-variable "key" { type = string }
-variable "sql" { type = string }
-variable "apps" { type = string }
-variable "pdns" { type = string }
-variable "lvmss" { type = string }
-variable "tenant" { type = string }
-variable "network" { type = string }
-variable "security" { type = string }
-variable "aspnetcore" { type = string }
-variable "environment" { type = string }
-variable "subscription" { type = string }
-
-variable "data" {
-  type    = list(string)
-  default = ["$ORGANIZATION", "public", "private"]
-}
-
-variable "group" {
-  type    = list(string)
-  default = ["vms", "apps", "storage", "networking"]
-}
-
-variable "node" {
-  type = list(string)
-  default = ["$ORGANIZATION", "ticket-manager", "storybook-$ORGANIZATION"
-  ]
-}
-
-variable "net" {
-  type    = list(string)
-  default = ["backoffice", "backoffice-api", "bff-integration-infra"]
-}
-
-variable "function" {
-  type = list(string)
-  default = ["marketing", "payments", "notification", "shipping-reports",
-  "shipping-dates", "customer-loyalty", "invoices-functions", "products-functions",
-  "shipping-experience", "back-office-functions"]
 }
